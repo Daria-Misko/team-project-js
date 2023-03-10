b7559a599301df64049b2d3f9b13cb9c24e01163
Daria-Misko <111460998+Daria-Misko@users.noreply.github.com>
2023-03-10 13:57:55 +0000

    Deploying to gh-pages from @ Daria-Misko/team-project-js@a8ca8624f004650316942156de295e32b5be9205 🚀

--- index.297da9e6.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("switch-style").onclick=function(){document.body.classList.toggle("dark-theme")};
-//# sourceMappingURL=index.297da9e6.js.map
--- index.666a4f1b.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("switch-style").onclick=function(){document.body.classList.toggle("dark-theme")};
-//# sourceMappingURL=index.666a4f1b.js.map
